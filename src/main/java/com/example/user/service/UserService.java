50c5fe9eceb6c0688511435a303e35aee07e2ac8
yadneysh <yadnyesh.juvekar@gmail.com>
2022-01-15 09:55:38 +0530

    Implemented return User with Department

--- src/main/java/com/example/user/service/UserService.java
+++ src/main/java/com/example/user/service/UserService.java
@@ -2,9 +2,12 @@ package com.example.user.service;
 
 import com.example.user.entity.User;
 import com.example.user.repository.UserRepository;
+import com.example.user.valueobjects.Department;
+import com.example.user.valueobjects.ResponseTemplateVO;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class UserService {
@@ -12,7 +15,20 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     public User saveUser(User user) {
         return userRepository.save(user);
+    }
+
+    public ResponseTemplateVO getUserByDepartment(Long userId) {
+        ResponseTemplateVO responseTemplateVO = new ResponseTemplateVO();
+        User user = userRepository.findByUserId(userId);
+        Department department = restTemplate
+                .getForObject("http://localhost:9001/departments/" + user.getDepartmentId(), Department.class);
+        responseTemplateVO.setDepartment(department);
+        responseTemplateVO.setUser(user);
+        return responseTemplateVO;
     }
 }
