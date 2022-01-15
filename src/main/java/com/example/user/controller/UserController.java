a05e16f2f076d2ff2ec72af9c95e1e57db6a2c5c
yadneysh <yadnyesh.juvekar@gmail.com>
2022-01-15 09:13:30 +0530

    Addinglog

--- src/main/java/com/example/user/controller/UserController.java
+++ src/main/java/com/example/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.user.controller;
 
 import com.example.user.entity.User;
 import com.example.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +12,17 @@ import javax.jws.soap.SOAPBinding;
 
 @RestController
 @RequestMapping("/users")
+@Slf4j
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     public User saveUser(@RequestBody User user) {
+        log.info("USER CONTROLLER - saveUser: " + user.toString());
         return userService.saveUser(user);
     }
+
+
 
 }
