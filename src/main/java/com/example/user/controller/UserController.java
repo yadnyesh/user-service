b6eca2dfd5bfca566c54bae82580480563c1b726
yadneysh <yadnyesh.juvekar@gmail.com>
2022-01-15 09:55:55 +0530

    Implemented return User with Department

--- src/main/java/com/example/user/controller/UserController.java
+++ src/main/java/com/example/user/controller/UserController.java
@@ -2,13 +2,10 @@ package com.example.user.controller;
 
 import com.example.user.entity.User;
 import com.example.user.service.UserService;
+import com.example.user.valueobjects.ResponseTemplateVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.jws.soap.SOAPBinding;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -18,11 +15,16 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @PostMapping("")
     public User saveUser(@RequestBody User user) {
         log.info("USER CONTROLLER - saveUser: " + user.toString());
         return userService.saveUser(user);
     }
 
+    @GetMapping("/{id}")
+    public ResponseTemplateVO getUserByDepartment(@PathVariable("id") Long userId) {
+        return userService.getUserByDepartment(userId);
+    }
 
 
 }
