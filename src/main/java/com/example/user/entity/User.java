34b50063c5c1275f30e3ee732db1c88b9a7eec57
Yadnyesh <yadnyesh.juvekar@walmart.com>
2022-01-15 09:03:01 +0530

    User Entity

---
+++ src/main/java/com/example/user/entity/User.java
@@ -0,0 +1,29 @@
+package com.example.user.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userId;
+
+    private String firstname;
+
+    private String lastname;
+
+    private String email;
+
+    private Long departmetId;
+}
